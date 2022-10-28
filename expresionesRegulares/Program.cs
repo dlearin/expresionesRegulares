17b1abd8f2192d48bc19b6c1d55a0fad522cc41d
carlos <cfernande1408@gmail.com>
2022-10-28 12:24:36 +0200

    Agregar archivos de proyecto.

---
+++ expresionesRegulares/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace expresionesRegulares
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string frase = "Mi nombre es Juan y mi n1 de tfno es (+34) 123-45-67 y mi codigo postal es 29679";       }
+    
+            
+    
+    }
+}
